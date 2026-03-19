fb36bf240b770b2b609ba9c9749df8da5839cd34
Matthew Reider <mreider@gmail.com>
2026-03-19 12:36:57 +0100

    Rename reference guide to OTel Primer in nav and mobile gate

--- site/js/progress.js
+++ site/js/progress.js
@@ -261,7 +261,7 @@
   var refLink = document.createElement('a');
   refLink.href = 'https://otel-primer.mreider.com';
   refLink.className = 'progress-icon';
-  refLink.title = t('common.referenceGuide', 'OpenTelemetry Reference Guide');
+  refLink.title = t('common.referenceGuide', 'OTel Primer');
   refLink.target = '_blank';
   refLink.rel = 'noopener';
   refLink.innerHTML = '<svg viewBox="0 0 16 16"><path d="M8.636 3.5a.5.5 0 00-.5-.5H1.5A1.5 1.5 0 000 4.5v10A1.5 1.5 0 001.5 16h10a1.5 1.5 0 001.5-1.5V7.864a.5.5 0 00-1 0V14.5a.5.5 0 01-.5.5h-10a.5.5 0 01-.5-.5v-10a.5.5 0 01.5-.5h6.636a.5.5 0 00.5-.5z"/><path d="M16 .5a.5.5 0 00-.5-.5h-5a.5.5 0 000 1h3.793L6.146 9.146a.5.5 0 10.708.708L15 1.707V5.5a.5.5 0 001 0v-5z"/></svg>';
@@ -298,7 +298,7 @@
     if (nextLink) nextLink.title = koanTitle(currentIdx + 1);
     trophyBtn.title = t('cert.shareTitle', 'Share Your Achievement');
     trophyBtn.setAttribute('aria-label', t('cert.shareTitle', 'Share Your Achievement'));
-    refLink.title = t('common.referenceGuide', 'OpenTelemetry Reference Guide');
+    refLink.title = t('common.referenceGuide', 'OTel Primer');
     ghLink.title = t('common.viewOnGithub', 'View on GitHub');
     copy.textContent = t('common.copyright', '\u00A9 2026 Matthew Reider \u00B7 MIT License \u00B7 v0.3.10');
     langBtn.title = t('common.changeLanguage', 'Change language');
@@ -328,7 +328,7 @@
     mobileRef.target = '_blank';
     mobileRef.rel = 'noopener';
     mobileRef.innerHTML = '<svg viewBox="0 0 16 16"><path d="M8.636 3.5a.5.5 0 00-.5-.5H1.5A1.5 1.5 0 000 4.5v10A1.5 1.5 0 001.5 16h10a1.5 1.5 0 001.5-1.5V7.864a.5.5 0 00-1 0V14.5a.5.5 0 01-.5.5h-10a.5.5 0 01-.5-.5v-10a.5.5 0 01.5-.5h6.636a.5.5 0 00.5-.5z"/><path d="M16 .5a.5.5 0 00-.5-.5h-5a.5.5 0 000 1h3.793L6.146 9.146a.5.5 0 10.708.708L15 1.707V5.5a.5.5 0 001 0v-5z"/></svg>' +
-      '<span>' + t('common.referenceGuide', 'Reference Guide') + '</span>';
+      '<span>' + t('common.referenceGuide', 'OTel Primer') + '</span>';
     gate.appendChild(mobileRef);
   }
 })();

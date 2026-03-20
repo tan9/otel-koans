3c4fc061d1c17a89a424eb8c186d1aa0e136a1a7
Pei-Tang Huang <tangtheone@gmail.com>
2026-03-20 16:18:03 +0800

    Fix nav bar icon style and sizing consistency
    - Replace trophy icon with Material Design Icons outlined emoji_events
    - Replace external link icon from Bootstrap (16x16, thin stroke) to MD open_in_new (24x24)
    - Standardize all icons to viewBox="0 0 24 24" matching hamburger and translate icons
    - Add explicit width/height to .trophy-toggle svg

--- site/js/progress.js
+++ site/js/progress.js
@@ -265,7 +265,7 @@
   refLink.title = t('common.referenceGuide', 'OTel Primer');
   refLink.target = '_blank';
   refLink.rel = 'noopener';
-  refLink.innerHTML = '<svg viewBox="0 0 16 16"><path d="M8.636 3.5a.5.5 0 00-.5-.5H1.5A1.5 1.5 0 000 4.5v10A1.5 1.5 0 001.5 16h10a1.5 1.5 0 001.5-1.5V7.864a.5.5 0 00-1 0V14.5a.5.5 0 01-.5.5h-10a.5.5 0 01-.5-.5v-10a.5.5 0 01.5-.5h6.636a.5.5 0 00.5-.5z"/><path d="M16 .5a.5.5 0 00-.5-.5h-5a.5.5 0 000 1h3.793L6.146 9.146a.5.5 0 10.708.708L15 1.707V5.5a.5.5 0 001 0v-5z"/></svg>';
+  refLink.innerHTML = '<svg viewBox="0 0 24 24"><path d="M14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7zM19 19H5V5h7V3H5a2 2 0 00-2 2v14a2 2 0 002 2h14a2 2 0 002-2v-7h-2v7z"/></svg>';
   nav.appendChild(refLink);
 
   // GitHub link
@@ -328,7 +328,7 @@
     mobileRef.className = 'mobile-gate-ref';
     mobileRef.target = '_blank';
     mobileRef.rel = 'noopener';
-    mobileRef.innerHTML = '<svg viewBox="0 0 16 16"><path d="M8.636 3.5a.5.5 0 00-.5-.5H1.5A1.5 1.5 0 000 4.5v10A1.5 1.5 0 001.5 16h10a1.5 1.5 0 001.5-1.5V7.864a.5.5 0 00-1 0V14.5a.5.5 0 01-.5.5h-10a.5.5 0 01-.5-.5v-10a.5.5 0 01.5-.5h6.636a.5.5 0 00.5-.5z"/><path d="M16 .5a.5.5 0 00-.5-.5h-5a.5.5 0 000 1h3.793L6.146 9.146a.5.5 0 10.708.708L15 1.707V5.5a.5.5 0 001 0v-5z"/></svg>' +
+    mobileRef.innerHTML = '<svg viewBox="0 0 24 24"><path d="M14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7zM19 19H5V5h7V3H5a2 2 0 00-2 2v14a2 2 0 002 2h14a2 2 0 002-2v-7h-2v7z"/></svg>' +
       '<span>' + t('common.referenceGuide', 'OTel Primer') + '</span>';
     gate.appendChild(mobileRef);
   }
